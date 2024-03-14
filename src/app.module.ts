a684de9011ae75b55656f6c758be94d32bb49cc7
gersonmlb <bazan.gerson.SAUL@gmail.com>
2024-03-14 02:03:45 -0500

    configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,16 @@ import { Module } from '@nestjs/common';
 import { CoreModule } from './core/core.module';
 import { InfraestructureModule } from './infraestructure/infraestructure.module';
 import { SharedModule } from './infraestructure/shared/shared.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
     InfraestructureModule,
     SharedModule,
-    CoreModule.register({ 
-      modules: [
-        InfraestructureModule
-      ] 
-    })
+    CoreModule.register({
+      modules: [InfraestructureModule],
+    }),
   ],
 })
-export class AppModule { }
+export class AppModule {}
