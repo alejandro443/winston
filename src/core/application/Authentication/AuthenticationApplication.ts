e4f2d660ca4092c0d61edc19a597e53d3af3a8fb
gersonmlb <bazan.gerson.SAUL@gmail.com>
2024-03-22 15:09:34 -0500

    prueba

--- src/core/application/Authentication/AuthenticationApplication.ts
+++ src/core/application/Authentication/AuthenticationApplication.ts
@@ -1,6 +1,6 @@
-import { LoginDto } from '@src/core/shared/dto/Authentication/authentication_dto';
-import { LoginResponseDto } from '@src/interfaces/request_dto/AuthDto/login.auth_dto';
+// import { LoginDto } from '@src/core/shared/dto/Authentication/authentication_dto';
+// import { LoginResponseDto } from '@src/interfaces/request_dto/AuthDto/login.auth_dto';
 
-export interface AuthenticationApplication {
-  loginAuth(authentication: LoginDto): Promise<LoginResponseDto>;
-}
+// export interface AuthenticationApplication {
+//   loginAuth(authentication: LoginDto): Promise<LoginResponseDto>;
+// }
--- src/core/service/Auth/AuthApplicationService.ts
+++ src/core/service/Auth/AuthApplicationService.ts
@@ -1,19 +1,19 @@
-import { AuthenticationApplication } from '@src/core/application/Authentication/AuthenticationApplication';
-import { LoginAuthUseCase } from './LoginAuthUseCase';
-import { LoginDto } from '@src/core/shared/dto/Authentication/authentication_dto';
+// import { AuthenticationApplication } from '@src/core/application/Authentication/AuthenticationApplication';
+// import { LoginAuthUseCase } from './LoginAuthUseCase';
+// import { LoginDto } from '@src/core/shared/dto/Authentication/authentication_dto';
 
-export class AuthenticationApplicationService
-  implements AuthenticationApplication
-{
-  constructor(private loginAuthUseCase?: LoginAuthUseCase) {
-    this.loginAuthUseCase = new LoginAuthUseCase();
-  }
+// export class AuthenticationApplicationService
+//   implements AuthenticationApplication
+// {
+//   constructor(private loginAuthUseCase?: LoginAuthUseCase) {
+//     this.loginAuthUseCase = new LoginAuthUseCase();
+//   }
 
-  async loginAuth(login: LoginDto) {
-    try {
-      return this.loginAuthUseCase.loginAuth(login);
-    } catch (error) {
-      return error;
-    }
-  }
-}
+//   async loginAuth(login: LoginDto) {
+//     try {
+//       return this.loginAuthUseCase.loginAuth(login);
+//     } catch (error) {
+//       return error;
+//     }
+//   }
+// }
