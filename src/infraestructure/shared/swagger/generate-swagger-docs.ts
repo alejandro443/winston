11f4fb0a6abfe5e0fd21d01c97dc2ab010fa978d
gersonmlb <bazan.gerson.SAUL@gmail.com>
2024-03-25 22:55:10 -0500

    add password

--- src/infraestructure/shared/swagger/generate-swagger-docs.ts
+++ src/infraestructure/shared/swagger/generate-swagger-docs.ts
@@ -1,15 +1,27 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
+
+const SWAGGER_ENVS = ['development'];
 
 export function GenerateSwaggerDocs(app: INestApplication) {
-  const config = new DocumentBuilder()
-    .setTitle('Fhyona Backend')
-    .setDescription(
-      'Proyecto de Fhyona 2.0 que integra muchas nuevas funcionalidades.',
-    )
-    .setVersion('2.0.0')
-    .build();
+  if (SWAGGER_ENVS.includes(process.env.NODE_ENV)) {
+    app.use(['/api-docs'], basicAuth({
+      challenge: true,
+      users: {
+        ['cudesi_development']: 'cudesi_development_2024',
+      },
+    }));
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+    const config = new DocumentBuilder()
+      .setTitle('Fhyona Backend')
+      .setDescription(
+        'Proyecto de Fhyona 2.0 que integra muchas nuevas funcionalidades.',
+      )
+      .setVersion('2.0.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, document);
+  }
 }
