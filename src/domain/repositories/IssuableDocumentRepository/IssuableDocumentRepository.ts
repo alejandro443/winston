704addad67bb681bd1bab0344dc057307992d0ee
gersonmlb <bazan.gerson.SAUL@gmail.com>
2024-09-25 11:31:10 -0500

    Update IssuableDocumentRepository.ts

--- src/domain/repositories/IssuableDocumentRepository/IssuableDocumentRepository.ts
+++ src/domain/repositories/IssuableDocumentRepository/IssuableDocumentRepository.ts
@@ -5,7 +5,7 @@ export class IssuableDocumentRepository {
 
   async findAll() {
     try {
-      return IssuableDocument.findAll({ where: { deleted_at: null } });
+      return IssuableDocument.findAll({ where: { deleted_at: null, status: true } });
     } catch (error: any) {
       return error;
     }
